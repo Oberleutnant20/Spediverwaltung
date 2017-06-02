cd5bda2590687680b5cc43932496a84f606f014f
Oberleutnant20 <toor1234@root>
2017-06-02 12:39:15 +0200

    testMain für die System Properties erstellt um zu wissen was alles über die Properties auslesbar ist.

--- Speditionsverwaltung/src/testMain.java
+++ Speditionsverwaltung/src/testMain.java
@@ -1,7 +1,4 @@
 
-import java.awt.SystemColor;
-import java.util.Properties;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
