29bc483ec562937a87204332308b4acc0a564598
Oberleutnant20 <toor1234@root>
2017-06-02 12:42:17 +0200

    Main einen Kommentar in Zeile 392 hinzugefügt

--- Speditionsverwaltung/src/de/uniqueplayers/speditionsverwaltung/gui/Main.java
+++ Speditionsverwaltung/src/de/uniqueplayers/speditionsverwaltung/gui/Main.java
@@ -388,7 +388,10 @@ public class Main extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        
+        //Logfile??
+        
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
